5b92abb6d9593874167d60a41db599fe749dc2c6
thanuscin <thanuscin@acumensolutions.com>
2018-12-12 17:53:59 -0500

    Added deploy and execute anon endpoints

--- app/routes/sfdx.js
+++ app/routes/sfdx.js
@@ -11,10 +11,44 @@ var path            = require('path');
 var util            = require('../lib/util');
 
 router.get('/authweblogin', function(req, res) {
-  var ls = spawn('sfdx force:auth:web:login',[''], {shell:true, cwd:`C:\\Users\\`});
+  var ls = spawn('sfdx force:auth:web:login -s',[''], {shell:true});
   ls.stdout.on('data', (data) => {
-	console.log(`stdout: ${data}`);
+	console.log(`Auth: stdout: ${data}`);
 	});
+  return res.sendStatus(200);
+});
+
+router.get('/deployclass/:file/:cwd', function(req, res) {
+  var deploy = spawn(`sfdx force:source:deploy -m ApexClass:${req.params.file}`,[''], {shell:true, cwd:`${req.params.cwd}`});
+
+  deploy.stdout.on('data', (data) => {
+    console.log(`Deploy: stdout: ${data}`);
+  });
+
+  deploy.stderr.on('data', (data) => {
+    console.log(`Deploy: stderr: ${data}`);
+  });
+  
+  deploy.on('close', (code) => {
+    console.log(`Deploy: child process exited with code ${code}`);
+  });
+  return res.sendStatus(200);
+});
+
+router.get('/executeanonymous/:codeChuck', function(req,res) {
+  var execAnon = spawn(`sfdx force:apex:execute << ${req.params.codeChunk}`,[''], {shell:true});
+
+  execAnon.stdout.on('data', (data) => {
+    console.log(`execAnon: stdout: ${data}`);
+  });
+
+  execAnon.stderr.on('data', (data) => {
+    console.log(`execAnon: stderr: ${data}`);
+  });
+  
+  execAnon.on('close', (code) => {
+    console.log(`execAnon: child process exited with code ${code}`);
+  });
   return res.sendStatus(200);
 });
 
