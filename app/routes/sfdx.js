9a1ecf90d176b484ffbc04de9a83a823ff104f7d
Ryan Faulhaber <faulhaber.14@osu.edu>
2018-12-12 20:03:49 -0500

    removed hard-coded paths

--- app/routes/sfdx.js
+++ app/routes/sfdx.js
@@ -3,26 +3,26 @@ const fs = require('fs');
 
 'use strict';
 
-var express         = require('express');
-var router          = express.Router();
-var inherits        = require('inherits');
-var logger          = require('winston');
-var querystring     = require('querystring');
-var path            = require('path');
-var util            = require('../lib/util');
+var express = require('express');
+var router = express.Router();
+var inherits = require('inherits');
+var logger = require('winston');
+var querystring = require('querystring');
+var path = require('path');
+var util = require('../lib/util');
 
-router.post('/authweblogin', function(req, res) {
-  var ls = spawn('sfdx force:auth:web:login -s',[''], {shell:true});
+router.post('/authweblogin', function (req, res) {
+  var ls = spawn('sfdx force:auth:web:login -s', [''], { shell: true });
   ls.stdout.on('data', (data) => {
-	console.log(`Auth: stdout: ${data}`);
-	});
+    console.log(`Auth: stdout: ${data}`);
+  });
   return res.sendStatus(200);
 });
 
-router.post('/deployclass', function(req, res) {
-  var deploy = spawn(`sfdx force:source:deploy -m ApexClass:${req.body.file}`,[], {
-    shell:true, 
-    cwd:`${req.body.cwd}`
+router.post('/deployclass', function (req, res) {
+  var deploy = spawn(`sfdx force:source:deploy -m ApexClass:${req.body.file}`, [], {
+    shell: true,
+    cwd: `${req.body.cwd}`
   });
 
   deploy.stdout.on('data', (data) => {
@@ -32,17 +32,18 @@ router.post('/deployclass', function(req, res) {
   deploy.stderr.on('data', (data) => {
     console.log(`Deploy: stderr: ${data}`);
   });
-  
+
   deploy.on('close', (code) => {
     console.log(`Deploy: child process exited with code ${code}`);
   });
   return res.sendStatus(200);
 });
 
-router.post('/executeanonymous', function(req,res) {
-  fs.writeFileSync("C:\\Users\\thanuscin\\Desktop\\MavensMate\\execAnon.txt", req.body.code);
+router.post('/executeanonymous', function (req, res) {
+  const tmpPath = path.join('.', 'tmp.cls');
+  fs.writeFileSync(path.join(tmpPath), req.body.code);
 
-  var execAnon = spawn(`sfdx force:apex:execute -f execAnon.txt`,[''], {shell:true});
+  var execAnon = spawn(`sfdx force:apex:execute -f ${tmpPath}`, [''], { shell: true });
 
   execAnon.stdout.on('data', (data) => {
     console.log(`execAnon: stdout: ${data}`);
@@ -51,9 +52,9 @@ router.post('/executeanonymous', function(req,res) {
   execAnon.stderr.on('data', (data) => {
     console.log(`execAnon: stderr: ${data}`);
   });
-  
+
   execAnon.on('close', (code) => {
-    fs.unlinkSync("C:\\Users\\thanuscin\\Desktop\\MavensMate\\execAnon.txt");
+    fs.unlinkSync(tmpPath);
     console.log(`execAnon: child process exited with code ${code}`);
   });
   return res.sendStatus(200);
